e4fb5ba8d0d6ee435f766ba9908d0bc6ce11f5b0
Nick Williams <nwilliams@xyme.ai>
2025-01-16 10:30:29 +0000

    using initliazie run

--- run_script/run_training.py
+++ run_script/run_training.py
@@ -1,22 +1,11 @@
 # Import standard libraries
 import os
-import jax
-import haiku as hk
-from functools import partial
 from absl import logging, app, flags
-import haiku.experimental.flax as hkflax
 
-from src.run.base import get_config
-from src.utils.base import initialize_training
-from src.utils.checkpoint.checkpoint_utils import find_highest_train_directory
-from src.utils.checkpoint.checkpoint_state import restore_or_initialize_state
-from src.utils.data.load_data import load_and_prepare_datasets
-from src.utils.containers import Graph
-from src.train.optimizer.optimizer import get_optimizer
+# Import custom libraries
 from src.train.scan.run_scan import run_scan
 from src.train.pmap.run_pmap import run_pmap
-from src.mlip.message_passing import MessagePassing
-from src.train.loss.loss import loss_fn_apply
+from src.run.base import initalize_run
 
 # Define flags 
 FLAGS = flags.FLAGS
@@ -51,65 +40,14 @@ flags.DEFINE_integer('num_basis_functions', 16, 'Number of basis functions')
 flags.DEFINE_float('cutoff', 5.0, 'Cutoff distance')
 flags.DEFINE_integer('max_atomic_number', 9, 'Maximum atomic number')
 
+flags.DEFINE_string('script_dir', os.path.dirname(os.path.abspath(__file__)), "Script directory.")
 
+# Set number of devics 
 os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=1"
 
 def main(argv):
-
-    config, n_devices = get_config(
-        FLAGS.data_dir,
-        FLAGS.dataset,
-        FLAGS.checkpoint_dir,
-        FLAGS.num_train,
-        FLAGS.num_valid
-        )
-
-    # Instantiate the message-passing model
-    message_passing = MessagePassing(
-        features=FLAGS.num_features,
-        max_degree=FLAGS.max_degree,
-        num_iterations=FLAGS.num_iterations,
-        num_basis_functions=FLAGS.num_basis_functions,
-        cutoff=FLAGS.cutoff,
-        max_atomic_number=FLAGS.max_atomic_number
-    )
-
-    # Define energy and force computation with Haiku model transformation
-    @hk.without_apply_rng
-    @hk.transform
-    def energy_and_forces(graph: Graph):
-        mod = hkflax.lift(message_passing, name='e3x_mlip')
-        return mod(graph.features, graph.positions)
-
-    # Initialize the optimizer
-    opt = get_optimizer(FLAGS.learning_rate, FLAGS.gradient_clipping)
-
-    # Restore or initialize the model state
-    state, start_epoch = restore_or_initialize_state(
-        key=config['init_key'],
-        model=energy_and_forces,
-        opt=opt,
-        data=config['samples_train'],
-        path=config['restore_checkpoint_dir'],
-        n_devices=config['n_devices']
-    )
-
-    # Set up paths and logging for training process
-    save_checkpoint_path = initialize_training(
-        state=state,
-        start_epoch=start_epoch,
-        num_epochs=FLAGS.num_epochs,
-        script_dir=os.path.dirname(os.path.abspath(__file__)),
-        logging_filename=FLAGS.logging_filename,
-        checkpoint_dir=FLAGS.checkpoint_dir
-    )
-
-    # Partial loss function with force weighting
-    loss_fn_partial = partial(
-        loss_fn_apply,
-        model_apply=energy_and_forces.apply,
-        forces_weight=FLAGS.forces_weight,
-    )
+    # Initialize run
+    config, n_devices, state, start_epoch, save_checkpoint_path, opt, loss_fn_partial = initalize_run(FLAGS)
 
     # Choose training method based on number of available devices
     if n_devices == 1:
@@ -127,7 +65,7 @@ def main(argv):
             checkpoint_update_freq=FLAGS.checkpoint_update_freq,
             batch_size=FLAGS.batch_size,
             output_keys=FLAGS.output_keys,
-            last_iter_info_only=False
+            last_iter_info_only=FLAGS.last_iter_info_only
         )
     
     elif n_devices > 1:
--- src/run/base.py
+++ src/run/base.py
@@ -1,16 +1,61 @@
+# Import standard libraries
 import os
 import jax
 import haiku as hk
+from functools import partial
+from absl import logging, flags
+import haiku.experimental.flax as hkflax
+import sys
+import chex
+from jax.flatten_util import ravel_pytree
 
+# Import custom libraries
 from src.utils.checkpoint.checkpoint_utils import find_highest_train_directory
 from src.utils.data.load_data import load_and_prepare_datasets
+from src.utils.checkpoint.checkpoint_state import restore_or_initialize_state
+from src.utils.containers import Graph
+from src.train.optimizer.optimizer import get_optimizer
+from src.mlip.message_passing import MessagePassing
+from src.train.loss.loss import loss_fn_apply
+from src.utils.containers import TrainingState
+from src.utils.setup_logging import setup_logging
+from src.utils.checkpoint.checkpoint_utils import create_training_directory
 
-def get_config(data_dir: str, 
-               dataset: str, 
-               checkpoint_dir: str,
-               num_train: int, 
-               num_valid: int
-               ):
+def param_count(x: chex.ArrayTree) -> int:
+    """Count the number of parameters in a PyTree of parameters."""
+    return ravel_pytree(x)[0].shape[0]
+
+# Initialize training process and directories
+def initialize_checkpoint_path(state: TrainingState, 
+                        start_epoch: int,
+                        num_epochs: int, 
+                        script_dir: str, 
+                        logging_filename: str, 
+                        checkpoint_dir: str):
+    
+    if start_epoch >= num_epochs:
+        print(f"Training already completed. Exiting. start_epoch: {start_epoch}, num_epochs: {num_epochs}")
+        sys.exit(0)
+
+    # Create directory for saving training checkpoints and set up logging
+    train_dir = create_training_directory(script_dir)
+    save_checkpoint_path = os.path.join(train_dir, checkpoint_dir)
+    setup_logging(train_dir, logging_filename)
+    logging.info(f"Starting epoch: {start_epoch}, params count: {param_count(state.params)}")
+
+    return save_checkpoint_path
+
+
+FLAGS = flags.FlagValues()
+
+def get_config(
+        script_dir: str,
+        data_dir: str, 
+        dataset: str, 
+        checkpoint_dir: str,
+        num_train: int, 
+        num_valid: int
+        ):
 
     # Initialize random number generator (RNG) keys for reproducibility
     rng_key, data_key, train_key, init_key = jax.random.split(jax.random.PRNGKey(0), 4)
@@ -23,7 +68,6 @@ def get_config(data_dir: str,
     print("Number of devices found: ", n_devices)
 
     # Find the highest training directory for resuming training
-    script_dir = os.path.dirname(os.path.abspath(__file__))
     highest_train_dir = find_highest_train_directory(script_dir)
     restore_checkpoint_dir = os.path.join(highest_train_dir, checkpoint_dir) if highest_train_dir else None
 
@@ -57,3 +101,82 @@ def get_config(data_dir: str,
         }
 
     return  config_dict, n_devices
+
+def initalize_run(FLAGS):
+    # Get configuration dictionary and number of devices
+    config, n_devices = get_config(
+        FLAGS.script_dir,
+        FLAGS.data_dir,
+        FLAGS.dataset,
+        FLAGS.checkpoint_dir,
+        FLAGS.num_train,
+        FLAGS.num_valid
+        )
+
+    # Instantiate the message-passing model
+    message_passing = MessagePassing(
+        features=FLAGS.num_features,
+        max_degree=FLAGS.max_degree,
+        num_iterations=FLAGS.num_iterations,
+        num_basis_functions=FLAGS.num_basis_functions,
+        cutoff=FLAGS.cutoff,
+        max_atomic_number=FLAGS.max_atomic_number
+    )
+
+    # Define energy and force computation with Haiku model transformation
+    @hk.without_apply_rng
+    @hk.transform
+    def energy_and_forces(graph: Graph):
+        mod = hkflax.lift(message_passing, name='e3x_mlip')
+        return mod(graph.features, graph.positions)
+
+    # Initialize the optimizer
+    opt = get_optimizer(FLAGS.learning_rate, FLAGS.gradient_clipping)
+
+    # Restore or initialize the model state
+    state, start_epoch = restore_or_initialize_state(
+        key=config['init_key'],
+        model=energy_and_forces,
+        opt=opt,
+        data=config['samples_train'],
+        path=config['restore_checkpoint_dir'],
+        n_devices=config['n_devices']
+    )
+
+    # Set up paths and logging for training process
+    save_checkpoint_path = initialize_checkpoint_path(
+        state=state,
+        start_epoch=start_epoch,
+        num_epochs=FLAGS.num_epochs,
+        script_dir=FLAGS.script_dir,
+        logging_filename=FLAGS.logging_filename,
+        checkpoint_dir=FLAGS.checkpoint_dir
+    )
+
+    # Partial loss function with force weighting
+    loss_fn_partial = partial(
+        loss_fn_apply,
+        model_apply=energy_and_forces.apply,
+        forces_weight=FLAGS.forces_weight,
+    )
+
+    return config, n_devices, state, start_epoch, save_checkpoint_path, opt, loss_fn_partial
+
+# class Run_Functions:
+#     def __init__(self., FLAGS):
+#         self.FLAGS = FLAGS
+#         self.config = None
+#         self.n_devices = None
+#         self.message_passing = None
+#         self.energy_and_forces = None
+#         self.opt = None
+#         self.state = None
+#         self.start_epoch = None
+#         self.save_checkpoint_path = None
+#         self.loss_fn_partial = None
+#         self.get_config()
+#         self.instantiate_model()
+#         self.define_model()
+#         self.init_optimizer()
+#         self.restore_or_initialize_state()
+#         self.initialize_training()
